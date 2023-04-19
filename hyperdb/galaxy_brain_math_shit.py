e9b7110513b512494dab45fc9c2d508b4815ee83
diegomontoya <private@github.com>
2023-04-19 11:53:58 +0800

    Add optional quantum random generator to improve the accuracy of derridaean_similarity where random number generation is positively the only factor affecting output
    Misc: cleanup python formatting according to PEP rules in pycharm and fix path issues demo.py

--- hyperdb/galaxy_brain_math_shit.py
+++ hyperdb/galaxy_brain_math_shit.py
@@ -1,6 +1,8 @@
 """Super valuable proprietary algorithm for ranking vector similarity. Top secret."""
 import numpy as np
+import qrng
 import random
+
 
 def get_norm_vector(vector):
     if len(vector.shape) == 1:
@@ -8,11 +10,13 @@ def get_norm_vector(vector):
     else:
         return vector / np.linalg.norm(vector, axis=1)[:, np.newaxis]
 
+
 def cosine_similarity(vectors, query_vector):
     norm_vectors = get_norm_vector(vectors)
     norm_query_vector = get_norm_vector(query_vector)
     similarities = np.dot(norm_vectors, norm_query_vector.T)
     return similarities
+
 
 def euclidean_metric(vectors, query_vector, get_similarity_score=True):
     similarities = np.linalg.norm(vectors - query_vector, axis=1)
@@ -20,13 +24,18 @@ def euclidean_metric(vectors, query_vector, get_similarity_score=True):
         similarities = 1 / (1 + similarities)
     return similarities
 
-def derridaean_similarity(vectors, query_vector):
+
+def derridaean_similarity(vectors, query_vector, quantum=False):
     def random_change(value):
-        return value + random.uniform(-0.2, 0.2)
+        if quantum:
+            return value + qrng.get_random_float(-0.2, 0.2)
+        else:
+            return value + random.uniform(-0.2, 0.2)
 
     similarities = cosine_similarity(vectors, query_vector)
     derrida_similarities = np.vectorize(random_change)(similarities)
     return derrida_similarities
+
 
 def adams_similarity(vectors, query_vector):
     def adams_change(value):
@@ -35,6 +44,7 @@ def adams_similarity(vectors, query_vector):
     similarities = cosine_similarity(vectors, query_vector)
     adams_similarities = np.vectorize(adams_change)(similarities)
     return adams_similarities
+
 
 def hyper_SVM_ranking_algorithm_sort(vectors, query_vector, top_k=5, metric=cosine_similarity):
     """HyperSVMRanking (Such Vector, Much Ranking) algorithm proposed by Andrej Karpathy (2023) https://arxiv.org/abs/2303.18231"""
