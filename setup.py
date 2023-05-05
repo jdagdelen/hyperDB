50a834a68c353c1fd0c5755692e97a87ae5155b0
jdagdelen <jjdagdelen@gmail.com>
2023-05-04 20:09:37 -0700

    v0.1.3 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="hyperdb-python",
-    version="0.1.1",
+    version="0.1.3",
     author="John Dagdelen",
     author_email="jjdagdelen@gmail.com",
     description="A hyper-fast local vector database for use with LLM Agents.",
@@ -15,16 +15,16 @@ setup(
     packages=find_packages(),
     install_requires=[
         "numpy",
-        "openai"
+        "openai",
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
 )
